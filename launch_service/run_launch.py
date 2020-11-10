b09729059fa5931fbbd8a5f52e7e6cf76b656cc1
Adi Bar Ilan <adi.perfetto@gmail.com>
2020-11-10 15:17:59 +0200

    Add kill and killall services

--- launch_service/run_launch.py
+++ launch_service/run_launch.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
 
 import rospy, subprocess
-from launch_service.srv import str
+from launch_service.srv import launchrequest, killrequest, killallrequest
 
-def launch_file(names):
+class LaunchService:
+    def __init__(self):
+        self.counter = 0
+        self.services = {}
 
-    command = ["roslaunch"] + names.data.split()
-    subprocess.Popen(command)
+        rospy.init_node('launch_service')
+        launch = rospy.Service('launch_service', launchrequest, self.launch_file)
+        kill = rospy.Service('kill_launch', killrequest, self.kill_process)
+        killall = rospy.Service('kill_all', killallrequest, self.kill_all)
+        print("Launch service running.")
+        rospy.spin()
+
+    def launch_file(self, data):
+        package, filename = data.package, data.file
+        command = ["roslaunch", package, filename]
+        self.services[self.counter] = subprocess.Popen(command)
+        self.counter += 1
+        return self.counter - 1
+
+    def kill_process(self, number):
+        try:
+            process = self.services[number.index]
+            process.terminate()
+            del self.services[number.index]
+        except:
+            return False
+        return True
+
+    def kill_all(self, _):
+        count = 0
+        for index in self.services.keys():
+            self.services[index].terminate()
+            del self.services[index]
+            count += 1
+        return count
+
 
 
 if __name__ == '__main__':
+    service_server = LaunchService()
 
-    rospy.init_node('launch_service')
-    s = rospy.Service('launch_service', str, launch_file)
-    print("Launch service running.")
-    rospy.spin()
+
