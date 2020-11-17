ade060a3de2a7e9eb43a924dc9e3a7b288124264
Adi Bar Ilan <adi.perfetto@gmail.com>
2020-11-17 09:07:41 +0200

    Add run

--- launch_service/run_launch.py
+++ launch_service/run_launch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy, subprocess
-from launch_service.srv import launchrequest, killrequest, killallrequest
+from launch_service.srv import launchrequest, killrequest, killallrequest, runrequest
 
 class LaunchService:
     def __init__(self):
@@ -10,14 +10,22 @@ class LaunchService:
 
         rospy.init_node('launch_service')
         launch = rospy.Service('launch_service', launchrequest, self.launch_file)
+        run = rospy.Service('run_service', runrequest, self.run_file)
         kill = rospy.Service('kill_launch', killrequest, self.kill_process)
         killall = rospy.Service('kill_all', killallrequest, self.kill_all)
         print("Launch service running.")
         rospy.spin()
 
     def launch_file(self, data):
-        package, filename = data.package, data.file
-        command = ["roslaunch", package, filename]
+        package, filename, args = data.package, data.file, data.args
+        command = ["roslaunch", package, filename, args]
+        self.services[self.counter] = subprocess.Popen(command)
+        self.counter += 1
+        return self.counter - 1
+
+    def run_file(self, data):
+        package, filename, args = data.package, data.file, data.args
+        command = ["rosrun", package, filename, args]
         self.services[self.counter] = subprocess.Popen(command)
         self.counter += 1
         return self.counter - 1
